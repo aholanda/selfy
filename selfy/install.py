298c24bbd4635ec9b642360ab7a0090d29e4d6b3
Adriano J. Holanda <ajholanda@gmail.com>
2021-11-16 23:17:35 -0300

    install ssh and use subprocess

--- selfy/install.py
+++ selfy/install.py
@@ -1,14 +1,33 @@
-import os
 import platform
+import subprocess
+import sys
+
+
+def exec(cmd):
+    print(cmd, file=sys.stderr, end='')
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+    return completed
+
+
+def ssh():
+    cmds = ['Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0\n',
+            'Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0\n']
+    for cmd in cmds:
+        exec(cmd)
+
+
+def choco_pkgs():
+    cur_os = platform.system()
+    fn = cur_os + '.install'
+    with open(fn) as f:
+        lines = f.readlines()
+        for line in lines:
+            cmd = 'choco install -y ' + line
+            exec(cmd)
+    
 
 class Install():
     @staticmethod
     def run():
-        cur_os = platform.system()
-        fn = cur_os + '.install'
-        with open(fn) as f:
-            lines = f.readlines()
-            for line in lines:
-                cmd = 'choco install -y ' + line
-                print(line)
-                os.system(cmd)
+        ssh()
+        choco_pkgs()
