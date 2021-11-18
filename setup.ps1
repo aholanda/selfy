a6f20b1c3af6c7e5ee56f7a0d12d73210d065327
Adriano J. Holanda <aholanda@usp.br>
2021-11-18 17:01:11 -0300

    install chocolatey and enable winrm

---
+++ setup.ps1
@@ -0,0 +1,16 @@
+$FileExists = Test-Path -Path 'C:\ProgramData\chocolatey\bin\choco.exe'
+If (-Not $FileExists) {
+    Set-ExecutionPolicy AllSigned
+    Write-Host "INFO: Installing Chocolatey..."
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+} else {
+    Write-Host "INFO: Chocolatey was already installed."
+}
+
+Write-Host "INFO: Enabling Windows Remote Management (WinRM)..."
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+powershell.exe -ExecutionPolicy ByPass -File $file
+Write-Host "ok"
