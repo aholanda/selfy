9c5704261a99a3c4e04285757d7ded2cadbffa54
Adriano J. Holanda <ajholanda@gmail.com>
2021-11-18 16:10:35 -0300

    refactor code to explorer dicts

--- selfy/install.py
+++ selfy/install.py
@@ -1,45 +1,33 @@
+import os
 import platform
 import subprocess
 import sys
+
+sudos = {'Linux': 'sudo', 'Windows': ''}
+pkg_mgrs = {'Linux': sudos['Linux'] + ' apt-get', 'Windows': 'choco'}
+install_cmds = {'Linux': pkg_mgrs['Linux'] + ' install -y ', 
+                'Windows': pkg_mgrs['Windows'] + ' install -y '}
+script_exts = {'Linux': '.sh', 'Windows': '.ps1'}
+
 
 def fatal(msg):
     print(msg, file=sys.stderr)
     sys.exit(1)
 
 
-def exec(plat, cmd_arr):
-    cmd = ' '.join(cmd_arr)
+def exec(cmd):
     print(cmd, file=sys.stderr)
-    if plat == 'Windows':
-        subprocess.run(cmd_arr, capture_output=True)
-    else:
-        subprocess.call(cmd, shell=True)
-
-
-def install_cmd(plat, args):
-    cmd = None
-    if plat == 'Windows':
-        if args == 'ssh':
-            return ['powershell', 
-                    '-Command', 
-                    'Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0']
-        elif args == 'sshd':
-            return ['powershell',  
-                    '-Command', 
-                    'Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0']
-        else:
-            cmd = 'choco install -y ' + args
-        return ["powershell", "-Command", cmd]
-    elif plat == 'Linux':
-        cmd = 'sudo apt-get install -y ' + args
-        return ["sudo", "", cmd]
-    else:
-        fatal('unknown platform "{}"'.format(plat))
+    os.system(cmd)
 
 
 def install(plat, args):
-    cmd_arr = install_cmd(plat, args)
-    exec(plat, cmd_arr)
+    script = os.path.join(plat, args + script_exts[plat])
+    if os.path.isfile(script):
+        exec(sudos[plat] + ' ' + script)
+        sys.exit(1)
+    else:
+        cmd = install_cmds[plat] + args
+        exec(cmd)
 
 
 def install_pkgs(plat):
@@ -47,6 +35,8 @@ def install_pkgs(plat):
     with open(fn) as f:
         lines = f.readlines()
         for pkg in lines:
+            if pkg[0] == '#':
+                continue
             pkg = pkg.strip()
             install(plat, pkg)
 
@@ -56,9 +46,10 @@ def update(plat, are_pkgs=False):
         update = 'update'
         if are_pkgs:
             update = 'upgrade'
-        exec(plat, ['sudo apt-get', update, '-y'])
+        exec(pkg_mgrs[plat] + update + '-y')
     else:
         print('WARNING: update is not implemented for Windows', file=sys.stderr)
+
 
 class Install():
     @staticmethod
