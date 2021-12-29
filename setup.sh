db51768fbeba9b84f559c3e207522c6491891ef4
Adriano J. Holanda <ajholanda@gmail.com>
2021-12-29 13:34:50 -0300

    create /etc/ansible if needed

--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@
 
 M='setup>'
 INSTALL='sudo apt-get install -y'
+ANSIBLE_DIR='/etc/ansible'
 
 # Setup basic Linux box
-${INSTALL} git gpg sudo wget ansible && sudo cp ./hosts /etc/ansible/
+${INSTALL} git gpg sudo wget ansible && \
+	if  [ ! -d ${ANSIBLE_DIR} ];then sudo mkdir ${ANSIBLE_DIR};fi &&\
+	      sudo cp ./hosts ${ANSIBLE_DIR}
 
 echo "$M set sudo group to not be prompted for password" 
 sudo sed -i 's/^%sudo\s*ALL=(ALL:ALL)\s*ALL/%sudo ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
