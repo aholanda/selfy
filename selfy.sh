e83fe7e775a7875a21ccfd8d93013eb2fb9940b3
Adriano J. Holanda <ajholanda@gmail.com>
2021-12-29 13:46:34 -0300

    add flag for office env setup

--- selfy.sh
+++ selfy.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
 case $1 in
-	--install)
+	--install-programming)
 		shift
-		ansible-playbook main.yml $@
+		ansible-playbook programming.yml $@
+		;;
+	--install-office)
+		shift
+		ansible-playbook office.yml $@
 		;;
 	*)
-		echo "usage: $0 --install"
+		>&2 echo "usage: $0 --install-office|--install-programming"
+		>&2 echo " --install-office"
+		>&2 echo "    Install packages to setup a basic office environment."
+		>&2 echo " --install-programming"
+		>&2 echo "    Install packages to setup a basic computer programming (not web) environment."
+		exit 1
 		;;
 esac
 
