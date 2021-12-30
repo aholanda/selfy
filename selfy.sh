784ffc4f3a8ac6b707ebb258e44210b2d1343294
Adriano J. Holanda <ajholanda@gmail.com>
2021-12-30 09:03:33 -0300

    include --install-webdev flag

--- selfy.sh
+++ selfy.sh
@@ -9,12 +9,20 @@ case $1 in
 		shift
 		ansible-playbook office.yml $@
 		;;
+	--install-webdev)
+		shift
+		ansible-playbook programming.yml --extra-vars "is_webdev_env=True" $@
+		;;
 	*)
-		>&2 echo "usage: $0 --install-office|--install-programming"
+		>&2 echo "usage: $0 --install-office|--install-programming <ANSIBLE-PLAYBOOK ARGS>"
 		>&2 echo " --install-office"
 		>&2 echo "    Install packages to setup a basic office environment."
 		>&2 echo " --install-programming"
 		>&2 echo "    Install packages to setup a basic computer programming (not web) environment."
+		>&2 echo " --install-webdev"
+		>&2 echo "    Install packages to setup a basic web development environment."
+		>&2 echo " <ANSIBLE-PLAYBOOK ARGS>"
+		>&2 echo "    All arguments accepted by ansible-playbook command."
 		exit 1
 		;;
 esac
